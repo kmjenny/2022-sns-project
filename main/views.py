2ae9a49049bdeafc019b9c8c541e088203a76d77
kmjenny <kjn3008@gmail.com>
2022-04-06 19:55:32 +0900

    과제 2번

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def showmain(request):
     return render(request, 'main/mainpage.html')
+
+def show_page(request):
+    return render(request, 'main/show.html')
