476e1a4221e0df675e4ef9587cac53922aba24d9
kmjenny <kjn3008@gmail.com>
2022-07-28 23:39:11 +0900

    과제

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 from asyncio import proactor_events
 from django.db import models
-from users.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
--- users/models.py
+++ users/models.py
@@ -1,7 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-class User(AbstractUser):
+class Profile(models.Model):
     followings = models.ManyToManyField("self", related_name = "followers", symmetrical=False)
     image = models.ImageField(upload_to="profile_img/", null=True, blank=True)
     info = models.TextField(null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
